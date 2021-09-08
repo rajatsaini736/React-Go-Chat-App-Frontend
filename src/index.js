bb580dfebae96449a5e0f5df74abe4bf79e02062
rajatsaini736 <rajatsaini736@gmail.com>
2021-09-08 22:58:40 +0530

    server added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let socket = new WebSocket("ws://localhost:8000/ws");
+// let socket = new WebSocket("ws://localhost:8000/ws");
+let socket = new WebSocket("ws://shrouded-ridge-25642.herokuapp.com/ws");
 
 let connect = (cb) => {
   console.log("Attempting Connection ... ");
